6259ebf90df1ab1ebb5de71549fd86c5141ca949
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-07-06 23:53:44 +0800

    Added GameState

--- src/models/game.rs
+++ src/models/game.rs
@@ -5,10 +5,29 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 
 #[derive(Debug)]
+pub enum GameState {
+    Init,
+    Started,
+    Ended,
+}
+
+#[derive(Debug)]
 pub struct Game {
     pub lobby_password: String,
     pub players: Vec<Player>,
     pub deck: Deck,
+    pub curr_state: GameState,
+}
+
+impl Game {
+    pub fn new(lobby_password: &str, players: Vec<Player>) -> Self {
+        Game {
+            lobby_password: lobby_password.to_string(),
+            players,
+            deck: Deck::new(),
+            curr_state: GameState::Init,
+        }
+    }
 }
 
 #[derive(Debug)]
